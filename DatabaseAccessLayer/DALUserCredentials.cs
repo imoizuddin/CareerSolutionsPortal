08238fb9edfb8e20c900f3709c34719160fd9b2d
Md Moizuddin Qureshi <Md.Moizuddin.Qureshi@ibm.com>
2021-08-31 10:59:41 +0530

    DAL user implemented

--- DatabaseAccessLayer/DALUserCredentials.cs
+++ DatabaseAccessLayer/DALUserCredentials.cs
@@ -13,112 +13,100 @@ namespace DatabaseAccessLayer
     {
         string connectionString = "Data Source=WIN-JIKA3OO75OT\\SQLEXPRESS;Initial Catalog=CareerSolutions;Integrated Security=True";
 
-        //public BALUserRegister FindUserDetails(int empid)
-        //{
-        //    SqlConnection cn = new SqlConnection(connectionString);
-        //    SqlCommand cmd = new SqlCommand("[dbo].[FindEmployee]", cn);
-        //    cmd.CommandType = CommandType.StoredProcedure;
+        public BALUserRegister FindUserDetails(int userid)
+        {
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("[dbo].[FindUser]", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
 
-        //    cn.Open();
-        //    cmd.Parameters.AddWithValue("@empid", empid);
+            cn.Open();
+            cmd.Parameters.AddWithValue("@userid", userid);
 
-        //    SqlParameter p1 = new SqlParameter();
-        //    p1.ParameterName = "@name";
-        //    p1.SqlDbType = SqlDbType.NVarChar;
-        //    p1.Size = 50;
-        //    p1.Direction = ParameterDirection.Output;
-        //    cmd.Parameters.Add(p1);
+            SqlParameter p1 = new SqlParameter();
+            p1.ParameterName = "@name";
+            p1.SqlDbType = SqlDbType.NVarChar;
+            p1.Size = 50;
+            p1.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(p1);
 
-        //    SqlParameter p2 = new SqlParameter();
-        //    p2.ParameterName = "@email";
-        //    p2.SqlDbType = SqlDbType.NVarChar;
-        //    p2.Direction = ParameterDirection.Output;
-        //    p2.Size = 50;
-        //    cmd.Parameters.Add(p2);
+            SqlParameter p2 = new SqlParameter();
+            p2.ParameterName = "@email";
+            p2.SqlDbType = SqlDbType.NVarChar;
+            p2.Direction = ParameterDirection.Output;
+            p2.Size = 50;
+            cmd.Parameters.Add(p2);
 
-        //    SqlParameter p3 = new SqlParameter();
-        //    p3.ParameterName = "@phone";
-        //    p3.SqlDbType = SqlDbType.BigInt;
-        //    p3.Direction = ParameterDirection.Output;
-        //    p3.Size = 50;
-        //    cmd.Parameters.Add(p3);
+            SqlParameter p3 = new SqlParameter();
+            p3.ParameterName = "@phone";
+            p3.SqlDbType = SqlDbType.BigInt;
+            p3.Direction = ParameterDirection.Output;
+            p3.Size = 50;
+            cmd.Parameters.Add(p3);
 
-        //    SqlParameter p4 = new SqlParameter();
-        //    p4.ParameterName = "@companyName";
-        //    p4.SqlDbType = SqlDbType.NVarChar;
-        //    p4.Direction = ParameterDirection.Output;
-        //    p4.Size = 50;
-        //    cmd.Parameters.Add(p4);
-
-
-        //    SqlDataReader dr = cmd.ExecuteReader();
-        //    BALEmployerRegister e = new BALEmployerRegister();
-        //    e.EmpId = empid;
-        //    e.FullName = cmd.Parameters["@name"].Value.ToString();
-        //    e.Email = cmd.Parameters["@email"].Value.ToString();
-        //    e.Phone = Convert.ToInt32(cmd.Parameters["@phone"].Value.ToString());
-        //    e.CompanyName = cmd.Parameters["@companyName"].Value.ToString();
+            SqlDataReader dr = cmd.ExecuteReader();
+            BALUserRegister e = new BALUserRegister();
+            e.UserId = userid;
+            e.FullName = cmd.Parameters["@name"].Value.ToString();
+            e.Email = cmd.Parameters["@email"].Value.ToString();
+            e.Phone = Convert.ToInt32(cmd.Parameters["@phone"].Value.ToString());
 
 
-        //    cn.Close();
-        //    cn.Dispose();
+            cn.Close();
+            cn.Dispose();
 
-        //    return e;
-        //}
+            return e;
+        }
 
-        //public void AddEmployer(BALEmployerRegister emp)
-        //{
-        //    SqlConnection cn = new SqlConnection(connectionString);
-        //    SqlCommand cmd = new SqlCommand("[dbo].[AddEmployee]", cn);
-        //    cmd.CommandType = CommandType.StoredProcedure;
-        //    string name = emp.FullName;
-        //    string email = emp.Email;
-        //    string pass = emp.Password;
-        //    int phone = emp.Phone;
-        //    string companyName = emp.CompanyName;
+        public void AddEmployer(BALUserRegister emp)
+        {
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("[dbo].[AddUser]", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            string name = emp.FullName;
+            string email = emp.Email;
+            string pass = emp.Password;
+            int phone = emp.Phone;
 
-        //    cmd.Parameters.AddWithValue("@name", name);
-        //    cmd.Parameters.AddWithValue("@email", email);
-        //    cmd.Parameters.AddWithValue("@password", pass);
-        //    cmd.Parameters.AddWithValue("@phone", phone);
-        //    cmd.Parameters.AddWithValue("@companyName", companyName);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@password", pass);
+            cmd.Parameters.AddWithValue("@phone", phone);
 
-        //    cn.Open();
-        //    cmd.ExecuteNonQuery();
+            cn.Open();
+            cmd.ExecuteNonQuery();
 
-        //    cn.Close();
-        //    cn.Dispose();
-        //}
+            cn.Close();
+            cn.Dispose();
+        }
 
 
-        //public void UpdateEmployer(BALEmployerRegister c)
-        //{
-        //    SqlConnection cn = new SqlConnection(connectionString);
-        //    SqlCommand cmd = new SqlCommand("UpdateCategoryData", cn);
-        //    cmd.CommandType = CommandType.StoredProcedure;
-        //    cmd.Parameters.AddWithValue("@empid", c.EmpId);
-        //    cmd.Parameters.AddWithValue("@name", c.FullName);
-        //    cmd.Parameters.AddWithValue("@email", c.Email);
-        //    cmd.Parameters.AddWithValue("@phone", c.Phone);
-        //    cmd.Parameters.AddWithValue("@companyName", c.CompanyName);
-        //    cn.Open();
-        //    cmd.ExecuteNonQuery();
-        //    cn.Close();
-        //    cn.Dispose();
-        //}
+        public void UpdateEmployer(BALUserRegister c)
+        {
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("UpdateUser", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@userid", c.UserId);
+            cmd.Parameters.AddWithValue("@name", c.FullName);
+            cmd.Parameters.AddWithValue("@email", c.Email);
+            cmd.Parameters.AddWithValue("@phone", c.Phone);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+        }
 
-        //public void DeleteEmployer(int id)
-        //{
+        public void DeleteEmployer(int id)
+        {
 
-        //    SqlConnection cn = new SqlConnection(connectionString);
-        //    SqlCommand cmd = new SqlCommand("RemoveCategory", cn);
-        //    cmd.CommandType = CommandType.StoredProcedure;
-        //    cmd.Parameters.AddWithValue("@empid", id);
-        //    cn.Open();
-        //    cmd.ExecuteNonQuery();
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("RemoveUser", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@userid", id);
+            cn.Open();
+            cmd.ExecuteNonQuery();
 
-        //    cn.Close();
-        //    cn.Dispose();
-        //}
+            cn.Close();
+            cn.Dispose();
+        }
     }
 }
